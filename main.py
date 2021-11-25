825e0ba64bd6a99f2fffd2359d2b8b270a47a239
shay-ehrlich <shayehr>
2021-11-25 13:14:36 +0200

    סעיף ד- רווח בר סמך

--- main.py
+++ main.py
@@ -88,31 +88,13 @@ if __name__ == '__main__':
     # 100 lottery every lottery diff seed
     rns.n = 500
     rns.len_of_uniform_array = 501
-    expectation_100 = pd.DataFrame(columns=['MC', 'MMR', 'RA', 'VHF_NAV', 'DME', 'RNS'])
-    number_of_trials = 100
-    for i in range(number_of_trials):
-        rns.seed = rns.set_seed()
-        distribution_dict_values = rns.randoms_array_for_sys(['MC', 'MMR', 'GPS ANT', 'LOC ANT Swi',
-                                                              'GS ANT', 'LOC ANT', 'RA', 'RA ANT', 'NAV-4000',
-                                                             'VOR ANT', 'MB ANT', 'ADF ANT', 'DME INT', 'ANT-42'])
-        curr_values = rns.compare_estimate()
-        expectation_100.loc[len(expectation_100)] = list(curr_values.values())
+    print(rns.confidence_interval())
 
-    #  Confidence interval 90%
-    bottom_decile = pd.DataFrame(columns=expectation_100.keys())
-    top_decile = pd.DataFrame(columns=expectation_100.keys())
-    for system in expectation_100.keys():
-        expectation_100[system] = sorted(expectation_100[system])
-        bottom_decile[system] = expectation_100[system].loc[0: 0.1 * number_of_trials - 1]
-        top_decile[system] = expectation_100[system].loc[0.9 * number_of_trials: number_of_trials]
 
-    merged_deciles = pd.concat([bottom_decile, top_decile])
-    # (1) Confidence interval by Empirical experiments
-    confidence_interval_empirical = stats.t.interval(0.9, len(merged_deciles) - 1, loc=np.mean(merged_deciles), scale= stats.sem(merged_deciles))
 
-    # (2) Confidence interval by normal distribution
-    confidence_interval_normal = stats.t.interval(0.9, len(expectation_100) - 1, loc=np.mean(expectation_100), scale= stats.sem(expectation_100))
-
+    # halton_num = rns.Halton_function(50)
+    halton_num = rns.Halton_function(5)
+    print(halton_num)
 
 
 
